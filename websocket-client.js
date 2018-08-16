db87204b8f04a4dd0a8dca4f22e2cc7da6c3f4f5
techraiders <hello_navneet@hotmail.com>
2018-08-16 12:39:51 +0530

    Creates WebSocket client in the browser sends user input data to WebSockerServer.

--- websocket-client.js
+++ websocket-client.js
@@ -1,1 +1,8 @@
 const WS = new WebSocket('ws://localhost:3232');
+
+document.forms[0].onsubmit = () => {
+  let input = document.getElementById('message');
+  console.log(input.value);
+
+  WS.send(input.value);
+};
--- websocket-server.js
+++ websocket-server.js
@@ -3,6 +3,10 @@ const WebSocketServer = WebSocket.Server;
 const WSS = new WebSocketServer({port: 3232});
 
 WSS.on('connection', (ws) => {
-  console.log('We are connected.');
+  //console.log('We are connected.');
+
+  ws.on('message', (message) => {
+    console.log(message);
+  });
 });
 
